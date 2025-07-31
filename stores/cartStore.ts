f69a9c6d6f74a5f46c7e56cbc0db8b0f3ac0fd95
Manie Events <manie.events@gmail.com>
2025-07-31 02:00:20 +0200

    recherche d ou provient la non suppresion du token

--- stores/cartStore.ts
+++ stores/cartStore.ts
@@ -1,11 +1,10 @@
 import type { BillingInfo } from '@/models/cart/billingInfo';
-import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useCartStore = defineStore('cartStore', () => {
-  const userTokenBalance = useLocalStorage('jeton-balance', 0);
-  const cartQuantity = useLocalStorage<number>('jeton-quantity', 0);
+  const userTokenBalance = ref<number>(0);
+  const cartQuantity = ref<number>(0);
   const addresses = ref<BillingInfo[]>([]);
 
   const getTotalPrice = computed(() => 9 * cartQuantity.value);
