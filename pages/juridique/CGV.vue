2950ce78fbdfb5aa811eb8eb0a72fc2d778d2027
Manie Events <manie.events@gmail.com>
2025-08-08 14:17:05 +0200

    rajout data cgv et integration

---
+++ pages/juridique/CGV.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel v-for="(item, index) in conditions" :key="index">
+      <v-expansion-panel-title>{{ item.title }}</v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <div v-html="item.description.replaceAll('\n', '<br>')"></div>
+
+        <v-expansion-panels v-if="item.children">
+          <v-expansion-panel v-for="(child, childIndex) in item.children" :key="childIndex">
+            <v-expansion-panel-title>{{ child.title }}</v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <div v-html="child.description.replaceAll('\n', '<br>')"></div>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+<script setup lang="ts">
+import conditions from '@/data/cgv.json';
+</script>
