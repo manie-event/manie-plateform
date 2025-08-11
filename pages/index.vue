fbb5ab4a06ab8f7af87c0fc693678e3d3ae50419
Manie Events <manie.events@gmail.com>
2025-08-12 00:35:28 +0200

    fix appel ping

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,13 @@ import Homepage from './front-pages/Homepage.vue';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { usePingService } from '../services/UsePingService';
+
+const { ping } = usePingService();
+
 onMounted(() => {
   AOS.init();
+  ping();
 });
 
 /*for Blank Layout*/
