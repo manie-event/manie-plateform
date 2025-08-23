3422577bf7b21c100a9b154807f3066582371a71
Manie Events <manie.events@gmail.com>
2025-08-23 18:22:14 +0200

    fix contactform

--- components/frontpages/ContactUs/ContactForm.vue
+++ components/frontpages/ContactUs/ContactForm.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import errorToaster from '@/components/common/errorToaster.vue';
 import type { ContactMessage } from '@/models/contact/contactMessage';
-import { useContactService } from '@/services/UseContactService.ts';
 import { ref } from 'vue';
+import { useContactService } from '~/services/UseContactService';
 
 const select = ref();
 const items = ref(['Question générale', 'Demande de rendez-vous', 'option 3', 'option 4']);
