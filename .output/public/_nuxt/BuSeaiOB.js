9d89286d1c3798e729e7ad4f5ffbae9f3955445e
Manie Events <manie.events@gmail.com>
2025-08-23 17:36:31 +0200

    npm run build

--- .output/public/_nuxt/BuSeaiOB.js
+++
@@ -1,1 +0,0 @@
-import{L as s}from"./RV8HHTPq.js";const p=s("/images/profile/user-1.jpg");export{p as _};
---
+++ .output/public/_nuxt/CgBOvGox.js
@@ -0,0 +1,1 @@
+import{N as s}from"./BGVbXBYx.js";const t=s("/images/products/empty-shopping-cart.svg");export{t as i};
