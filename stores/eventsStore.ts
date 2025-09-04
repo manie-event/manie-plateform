4deac8938363e9a4ae2d5e366809d37be4bfe4ee
Manie Events <manie.events@gmail.com>
2025-09-04 22:41:00 +0200

    navigation dashboard

--- stores/eventsStore.ts
+++ stores/eventsStore.ts
@@ -8,11 +8,9 @@ export const eventsStore = defineStore('eventsStore', () => {
 
   const setEventsByOrganisator = (newEvents: eventModel[]) => {
     events.value = newEvents;
-    console.log(events.value, 'EVENTS');
   };
   const setQuestionnaireAnswers = (newAnswers: eventModel) => {
     answers.value = newAnswers;
-    console.log(answers.value, 'answers VALUE');
   };
   return { events, answers, setEventsByOrganisator, setQuestionnaireAnswers };
 });
