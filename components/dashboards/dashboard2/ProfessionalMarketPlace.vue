6eac179f4714315fda1dda7f4412816421f08456
Manie Events <manie.events@gmail.com>
2025-09-29 00:25:02 +0530

    gestion chargement profil

--- components/dashboards/dashboard2/ProfessionalMarketPlace.vue
+++ components/dashboards/dashboard2/ProfessionalMarketPlace.vue
@@ -1,22 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
-  <v-dialog v-model="openModal" max-width="800">
-    <template #activator="{ props }">
-      <v-btn v-bind="props">Open Dialog</v-btn>
-    </template>
+  <v-dialog v-model="openPropositionPro" max-width="800">
     <v-card>
       <v-card-title class="text-h5">Professional Market Place</v-card-title>
       <v-card-text>
         <p>This is a dialog content.</p>
       </v-card-text>
       <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text @click="openModal = false">Close</v-btn>
+        <v-spacer />
+        <v-btn text @click="openPropositionPro = false">Close</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
+
 <script setup lang="ts">
-const openModal = defineModel<boolean>('openMarketModal', { default: false });
+const openPropositionPro = defineModel<boolean>('openPropositionPro', { default: false });
 </script>
