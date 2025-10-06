60ef918b4a30db2fc07a345aff8bccb99bf067f8
Manie Events <manie.events@gmail.com>
2025-10-06 22:28:42 +0200

    fix: configure Vercel for SPA routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,51 +1,36 @@
-import { fileURLToPath } from 'url';
 import svgLoader from 'vite-svg-loader';
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   runtimeConfig: {
     public: {
       tokenStripe: process.env.TOKEN_STRIPE,
       apiUrl: process.env.NUXT_API_URL || 'https://manie-api.onrender.com',
     },
   },
-  alias: {
-    '@': fileURLToPath(new URL('./', import.meta.url)), // racine projet
-    '@components': fileURLToPath(new URL('./components', import.meta.url)), // exemple
-  },
-  // TypeScript configuration
-  typescript: {
-    shim: false,
+
+  nitro: {
+    preset: 'vercel-static',
+    serveStatic: true,
   },
 
-  // Vuetify build configuration
+  // Le reste de votre config...
   build: {
     transpile: ['vuetify', 'vee-validate'],
   },
 
-  //Plugins
   vite: {
     plugins: [svgLoader()],
   },
 
-  // Modules
   modules: ['@pinia/nuxt'],
 
-  // Application metadata
   app: {
     head: {
       title: 'Manie - Facilite vos événements',
     },
   },
-
-  // Nitro configuration
-  nitro: {
-    serveStatic: true,
-  },
-
-  // Dev server handlers
-  devServerHandlers: [],
 
   compatibilityDate: '2024-09-06',
 });
