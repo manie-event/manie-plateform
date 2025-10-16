f3c9109e586ed09e9197d5d7be9ad58b18ac1e14
Manie Events <manie.events@gmail.com>
2025-10-16 13:26:32 +0200

    ajout google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,12 +24,20 @@ export default defineNuxtConfig({
     plugins: [svgLoader()],
   },
 
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/gtm'],
 
   app: {
     head: {
       title: 'Manie - Facilite vos événements',
     },
+  },
+
+  gtm: {
+    id: 'GTM-5V625W39',
+    enabled: true,
+    debug: false,
+    loadScript: true,
+    pageTracking: true,
   },
 
   compatibilityDate: '2024-09-06',
