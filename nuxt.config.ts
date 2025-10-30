04afdfde557b3dbd95f11bbb40273f3d5cd4586f
Manie Events <manie.events@gmail.com>
2025-10-30 12:47:36 +0100

    fix build vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import svgLoader from 'vite-svg-loader';
 
+process.env.NUXT_TELEMETRY_DISABLED = '1'
+
 export default defineNuxtConfig({
   ssr: false,
 
@@ -8,6 +10,8 @@ export default defineNuxtConfig({
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'https://manie-api.onrender.com',
     },
   },
+  telemetry: false,
+  devtools: { enabled: false },
 
   nitro: {
     preset: 'vercel',
