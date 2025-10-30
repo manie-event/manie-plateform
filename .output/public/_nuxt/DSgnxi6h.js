f2365d7e0e540c6defa9e124d097bdb8dec92a18
Manie Events <manie.events@gmail.com>
2025-10-30 16:16:30 +0100

    test build vercel

--- .output/public/_nuxt/DSgnxi6h.js
+++
@@ -1,1 +0,0 @@
-import{O as s}from"./Y2cM3ybq.js";const i=s("/images/backgrounds/login-security.png");export{i as _};
--- .vercel/output/static/_nuxt/C1qcQb_q.js
+++
@@ -1,1 +0,0 @@
-import"./C7zyImg8.js";const p=""+new URL("marcos-paulo-prado-0pOlBhSsF80-unsplash.1R_LMGxm.jpg",import.meta.url).href;export{p as n};
