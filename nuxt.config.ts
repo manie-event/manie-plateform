14538fd7375ff35473aafda7cd97c1e0eac2f701
Manie Events <manie.events@gmail.com>
2025-10-30 13:18:01 +0100

    suppression lib package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 import svgLoader from 'vite-svg-loader';
 
-process.env.NUXT_TELEMETRY_DISABLED = '1'
+// process.env.NUXT_TELEMETRY_DISABLED = '1'
 
 export default defineNuxtConfig({
   ssr: false,
@@ -10,10 +10,11 @@ export default defineNuxtConfig({
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'https://manie-api.onrender.com',
     },
   },
-  telemetry: false,
-  devtools: { enabled: false },
+  // telemetry: false,
+  // devtools: { enabled: false },
 
   nitro: {
+    sqlite: false,
     preset: 'vercel',
   },
 
